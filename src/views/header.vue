049ec767329a1cce699711051b4f134593f9472e
unknown <n.niedziela201@gmail.com>
2021-07-09 19:05:56 +0200

    add scroling

--- src/views/header.vue
+++ src/views/header.vue
@@ -1,12 +1,12 @@
 <template>
 <header class="header d-flex flex-column">
         <ul class="nav__list d-flex justify-center mb-auto">
-            <li class="nav__list__item"><a href="#Header" class="link">HOME</a></li>
-            <li class="nav__list__item"><a href="#" class="link">SHOP</a></li>
-            <li class="nav__list__item"><a href="#About" class="link">ABOUT</a></li>
-            <li class="nav__list__item"><a href="#Services" class="link">SERVICES</a></li>
-            <li class="nav__list__item"><a href="#Galery" class="link">GALERY</a></li>
-            <li class="nav__list__item"><a href="#Contact" class="link">CONTACT</a></li>
+            <li class="nav__list__item link" @click="$vuetify.goTo('#Header', ease)">HOME</li>
+            <li class="nav__list__item link" @click="$vuetify.goTo('#Header', ease)">SHOP</li>
+            <li class="nav__list__item link" @click="$vuetify.goTo('#About', ease)">ABOUT</li>
+            <li class="nav__list__item link" @click="$vuetify.goTo('#Services', ease)">SERVICES</li>
+            <li class="nav__list__item link" @click="$vuetify.goTo('#Galery', ease)">GALERY</li>
+            <li class="nav__list__item link" @click="$vuetify.goTo('#Contact', ease)">CONTACT</li>
         </ul>
         <article class="d-flex flex-column align-center align-self-center mb-auto">
         <h1 class="heading heading--header">Lorem ipsum</h1>
