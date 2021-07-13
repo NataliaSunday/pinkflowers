a4b373af0bf83cc1cd6a276f2ab35a9c0d42df61
unknown <n.niedziela201@gmail.com>
2021-07-13 17:49:18 +0200

    add services data (json)

--- src/views/services.vue
+++ src/views/services.vue
@@ -5,15 +5,23 @@
 </template>
 <script>
 import Heading from '../components/sectionHeadings.vue' 
+import servicePanel from '../components/servicePanel.vue'
 export default {
     data(){
         return{
             title: "Services",
-            headingColor: "heading--pink"
+            headingColor: "heading--pink",
+            services: {
+                name: 'bouquest',
+                imgPath: '../assets/bouquest.jpg',
+                description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia '
+
+            }
         }
     },
     components:{
-        Heading
+        Heading,
+        servicePanel
     }
 }
 </script>
