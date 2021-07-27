a143c89a1501d1d6e7efe2eabb0c41ef9752f242
unknown <n.niedziela201@gmail.com>
2021-07-27 19:45:52 +0200

    add components and viwe

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,12 +1,28 @@
 <template>
-    <div>
-        <p>Contact</p>
-    </div>
+    <section class="contact bg-light-pink pb-16">
+         <Heading :sectionTitle=title :headingColor=headingColor></Heading>
+         <ContactForm></ContactForm>
+    </section>
 </template>
-<style scoped>
-p{
+<script>
+import Heading from '../components/sectionHeadings.vue'
+import ContactForm from '../components/contactForm.vue'
+export default {
+  data () {
+    return {
+      title: 'Contact',
+      headingColor: 'heading--white',
+    }
+  },
+  components: {
+    Heading,
+    ContactForm,
+  }
+}
+</script>
+<style lang="scss" scoped>
+.contact{
     width: 100vw;
     height: 100vh;
-    background-color: palegoldenrod;
 }
 </style>
